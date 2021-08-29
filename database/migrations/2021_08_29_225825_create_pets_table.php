d61ffe78ab20ecd90878ef4522b61f2702ef65d8
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-08-29 20:05:17 -0300

    Criação das migrations

---
+++ database/migrations/2021_08_29_225825_create_pets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('integer');
+            $table->string('breed');
+
+            $table->foreignId('classification_id')
+                ->constrained();
+
+            $table->foreignId('owner_id')
+                ->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
