c183cb0d9b3c471aa4076bbee88d10b120bcc007
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-08-29 17:06:59 -0300

    first commit

---
+++ app/Models/Pet.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['name','age','breed'];
+
+    public function owner()
+    {
+        return $this->belognsTo(Owner::class);
+    }
+
+    public function classification()
+    {
+        return $this->belognsTo(Classification::class);
+    }
+}
+?>
+
+
